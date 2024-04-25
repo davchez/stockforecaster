055bb21216a609bd4e7cec23f9c1dcfd330b8c36
David Sanchez <davids1lite@gmail.com>
2024-04-25 14:27:57 -0700

    Pre-release v.1.20
    Major updates
    - Formula updated to 1 / ((alpha * training RMSE) + (1 - alpha)(validation RMSE)) so that highest score = best epoch
    - Improved runtime considerably by removing redundant and unnecessary code
    - Simplified formula calculation process
    - Added Alphabet/Google stock prediction confirmation data
    - Improved README.md formatting
    - Added comments to most lines, still working on it
    - Data visualization major changes, loss chart updated to reflect RMSE and not just MSE (to show how score works)
    
    Notes for next update
    - Continue making code as succinct as possible
    - Add formatting if investiny and investpy fail
    - Calculate accuracy function to prove model reliability (or lack thereof)

--- README.md
+++ README.md
@@ -1,48 +1,46 @@
-Title: Stock Price Predictor
-Author: David Sanchez
-GitHub: https://github.com/davchez
+<h3>Description:</h3>
+This machine learning Python project leverages the neural network capabilities of the TensorFlow and Keras packages.  It formats historic stock prices into time series data to train a long short-term memory (LSTM) model.  The model predicts stock price movements x amount of days into the future; it generally works best with predictions spanning 20 days and stock history spanning just over 1 year.
 
-Description:
-    This machine learning Python project leverages the neural network capabilities of the TensorFlow
-    and Keras packages.  It formats historic stock prices into time series data to train a long short-term
-    memory (LSTM) model.  The model predicts stock price movements x amount of days into the future;
-    it generally works best with predictions spanning 20 days.
+The model allows 15 epochs of the entire data set, which, according to scientific literature found in https://www.geeksforgeeks.org, is enough to train the model and to prevent overfitting.
 
-    The model allows 15 epochs of the entire data set, which, according to scientific literature
-    found in https://www.geeksforgeeks.org, is enough to train the model and to prevent
-    overfitting.
+<h3>Bugs:</h3>
+- Investpy and Investiny packages are not entirely reliable.
+- Length of necessary stock data must be longer than 5 months for program to work.
 
-Bugs: 
-    Investpy and Investiny packages are not entirely reliable.
-    Length of necessary stock data must be longer than 5 months for program to work.
+<h3>Credits:</h3>
+Caelan from Kite on YouTube.  Assistance in developing code for the neural network model.
 
-Credits:
-    Caelan from Kite on YouTube.  Assistance in developing code for the neural network model.
+<h3>Citation:</h3>
+Alvaro Bartolome del Canto. investpy - Financial Data Extraction from Investing.com with Python. 2018-2021. GitHub Repository. Available at: https://github.com/alvarobartt/investpy
 
-Citation:
-    Alvaro Bartolome del Canto. investpy - Financial Data Extraction from Investing.com with Python.
-    2018-2021. GitHub Repository. Available at: https://github.com/alvarobartt/investpy
+---
 
-Notes:
-    Small observation made is that the model seems to work well with stock data just over one year.
+<h1>Reproducible results: Confirmation samples with accuracy score</h1>
+> - Confirmation results following update of model epoch scoring formula
 
-Picture below shows Apple stock prediction with the following statistics
-- Actual April 24, 2024 close: $169.02 USD (downtrend)
-- Predicted April 24, 2024 close: $169.71 USD (predicted downtrend)
-- Training and validation data spanning January 1, 2023 to March 27, 2023 (20 trading days before April 24)
-- Model forecasted 20-day price accuracy: 99.59% (pre Q1-earnings)
+<h3>Apple (AAPL) Prediction (99.59% Accuracy)</h3>
+> - **Actual April 24, 2024 close: $169.02 USD** (downtrend)
+> - **Predicted April 24, 2024 close: $169.71 USD** (predicted downtrend)
+> - Training and validation data spanning January 1, 2023 to March 27, 2023 (20 trading days before April 24)
+> - Model forecasted 20-day price accuracy: 99.59% (pre Q1-earnings)
 ![Image](stockdata/sample_outputs/confirmation_outputs/AAPL_Sample_Output.png)
 
-Picture below shows Disney stock prediction with the following statistics
-- Actual April 24, 2024 close: $113.92 USD (downtrend)
-- Predicted April 24, 2024 close: $111.93 USD (predicted downtrend)
-- Training and validation data spanning January 1, 2023 to March 27, 2023 (20 trading days before April 24)
-- Model forecasted 20-day price accuracy: 98.25% (post Q1-earnings)
+<h3>Disney (DIS) Prediction (98.25% Accuracy)</h3>
+> - **Actual April 24, 2024 close: $113.92 USD** (downtrend)
+> - **Predicted April 24, 2024 close: $111.93 USD** (predicted downtrend)
+> - Training and validation data spanning January 1, 2023 to March 27, 2023 (20 trading days before April 24)
+> - Model forecasted 20-day price accuracy: 98.25% (post Q1-earnings)
 ![Image](stockdata/sample_outputs/confirmation_outputs/DIS_Sample_Output.png)
 
-Picture below shows Oracle stock prediction with the following statistics
-- Actual April 24, 2024 close: $115.34 USD (downtrend)
-- Predicted April 24, 2024 close: $123.39 USD (downtrend)
-- Training and validation data spanning January 1, 2023 to March 27, 2023 (20 trading days before April 24)
-- Model forecasted 20-day price accuracy: 93.02% (post Q1-earnings)
+<h3>Oracle (ORCL) Prediction (93.02% Accuracy)</h3>
+> - **Actual April 24, 2024 close: $115.34 USD** (downtrend)
+> - **Predicted April 24, 2024 close: $123.39 USD** (predicted downtrend)
+> - Training and validation data spanning January 1, 2023 to March 27, 2023 (20 trading days before April 24)
+> - Model forecasted 20-day price accuracy: 93.02% (post Q1-earnings)
 ![Image](stockdata/sample_outputs/confirmation_outputs/ORCL_Sample_Output.png)
+
+<h3>Alphabet/Google (GOOG) Prediction (95.54% Accuracy)</h3>
+> - **Actual April 24, 2024 close: $161.10 USD (uptrend)**
+> - **Predicted April 24, 2024 close: $153.92 USD (predicted uptrend)**
+> - Training and validation data spanning January 1, 2023 to March 27, 2023 (20 trading days before April 24)
+> - Model forecasted 20-day price accuracy: 95.54% (pre Q1-earnings)
--- stockforecaster.py
+++ stockforecaster.py
@@ -27,35 +27,37 @@ Citation:
 Notes:
     Small observation made is that the model seems to work well with stock data just over one year.
 """
-
+import investiny # Necessary for scraping Investing.com
+import investpy # Necessary for scraping Investing.com
+import glob 
+import os
 import pandas as pd
 import numpy as np
-import investiny
-import investpy
-import glob
-import os
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras.models import load_model
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Dropout
+from keras.callbacks import ModelCheckpoint
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler # Necesssary to constrict range to improve runtime
 
 DAYS_AHEAD = 20 # Predicting 20 days ahead
 WINDOW_SIZE = 20 # Size of window (20 days)
-DIRECTORY = 'saved_models/'
+DIRECTORY = 'saved_models/' 
 PATH = DIRECTORY + 'model_epoch_*.keras'
+UNUSED_MSG = 'N/A' # If investiny and investpy fail
 
-date_format = 'Unused, default data used'
-
-epochs = glob.glob( PATH )
+date_format, start_date, end_date = UNUSED_MSG, UNUSED_MSG, UNUSED_MSG
+epochs = glob.glob( PATH ) # List of old model epochs
 
 # Wipes out saved models folder before proceeding
-for epoch in epochs:
-    os.remove( epoch )
+for epoch in epochs: 
+    os.remove( epoch ) # Removes all old model epochs, if they exist
 
-# Symbol for stock ticker ( i.e. if Apple, enter AAPL )
-symbol = input( "Enter stock symbol: " )
-
-# Exchange that stock is publicly traded on ( i.e. NASDAQ )
-exchange = input( "Enter stock exchange: " )
-
-# Default CSV file of AAPL data will load if investiny/investpy fail
-default = False
+symbol = input( "Enter stock symbol: " ) # Symbol for stock ticker ( i.e. if Apple, enter AAPL )
+exchange = input( "Enter stock exchange: " ) # Exchange that stock is publicly traded on ( i.e. NASDAQ )
+default = False # Default CSV file in stockdata will load if investiny/investpy fail
 
 try:
     search_result = investiny.search_assets( query = symbol, # Searches Investing.com database to see if query exists
@@ -95,8 +97,9 @@ if default == False: # If investiny/investpy worked
           END: { end_date }\n""" )
     df = historical_data.reset_index() # Ensuring that indices are formatted correctly before analysis/tidying
 else: # IF investiny/investpy failed
-    symbol = 'RIVN'
+    symbol = 'AAPL' # Default stock to be used
     df = pd.read_csv( f'stockdata/{ symbol }_Stock_Data.csv' )
+    
 
 # Clean data and prepare for analysis
 df.columns = df.columns.str.lower() # Lowercasing all column labels in the DataFrame
@@ -107,8 +110,6 @@ df = df.reset_index( drop = True ) # Removes original indices so that only the r
 
 prices_df = df.astype( 'float32' ) # Converts all price data to 32 digit float values
 prices_df = np.reshape( prices_df, ( -1, 1 ) ) # Reshapes data into necessary rows and one column
-
-from sklearn.preprocessing import MinMaxScaler # Necesssary to constrict range to improve runtime
 
 scaler = MinMaxScaler( feature_range = ( 0, 1 ) ) # Reshapes data into variable "scaler" so that it shrinks all values of data into range between 0 and 1
 
@@ -153,9 +154,9 @@ tuple of ndarray
 """
 def slidingWindow( input, window_size ):
 
-    WINDOW, TARGET = [], []
+    WINDOW, TARGET = [], [] 
 
-    for i in range( ( len( input ) - 1 ) - window_size ):
+    for i in range( ( len( input ) - 1 ) - window_size ): # Number of windows to be generated
 
         window = input[ i:( i + window_size ), 0 ] # Creates windows of size "WINDOW_SIZE"
         WINDOW.append( window ) # Appends windows into WINDOW array
@@ -163,64 +164,47 @@ def slidingWindow( input, window_size ):
 
     return np.array( WINDOW ), np.array( TARGET ) # Creating numpy arrays WITHOUT INDICES which contain necessary data points (thus, returns tuples)
 
-# The first 80% of the data is parsed into 20 data points with an associated target point; this is repeated throughout the first 80% of the dataset
-training_window, training_target = slidingWindow( training_df, WINDOW_SIZE )
-
-# The same is made for the second 20% of the data
-val_window, val_target = slidingWindow( val_df, WINDOW_SIZE )
+training_window, training_target = slidingWindow( training_df, WINDOW_SIZE ) # Training array of 20-day windows with corresponding prediction target 
+val_window, val_target = slidingWindow( val_df, WINDOW_SIZE ) # Validation array of 20-day windows with corresponding prediction target
 
 # Reformatting train_window into time-series data: Each 20-day sliding window training sample is being classified into a single time step
 training_window = np.reshape( training_window, 
                             ( training_window.shape[ 0 ], 1, training_window.shape[ 1 ] ) )
 
-# Same idea
+# Reformatting val_window into time-series data: Each 20-day sliding window training sample is being classified into a single time step
 val_window = np.reshape( val_window,
                            ( val_window.shape[ 0 ], 1, val_window.shape[ 1 ] ) )
 
-# Shape of the data variable, which SHOULD be the sum of the samples (rows) of the training + test division (80% + 20%)
-prices_dimensions = prices_df.shape
-
-# First 80% of the data
-training_df_dimensions = training_df.shape
-
-# Last 20% of the data
-val_df_dimensions = val_df.shape
+prices_dimensions = prices_df.shape # Variable to check if data leak occurred (i.e. dimension concatenation error)
+training_df_dimensions = training_df.shape # First 80% of the data
+val_df_dimensions = val_df.shape # Remaining 20% of the data
 
 # Checks if any entry was repeated or missing as a result of the splitting (or, alternatively, an accidental merge): data leak
 def dataLeakOccured( merge_shape, shape_1, shape_2 ):
     return not( merge_shape[0] == ( shape_1[ 0 ] + shape_2[ 0 ] ) )
 
-# Prints false if data leak did not occur; true if otherwise
 dataLeak = dataLeakOccured( prices_dimensions, training_df_dimensions, val_df_dimensions )
 if dataLeak:
     print("Data leak HAS occurred.\nPlease check data as final result will likely be affected.\n")
 else:
     print("Data leak has NOT occurred.\nTraining and validation phases were partitioned correctly.\n")
 
-import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout
-from keras.callbacks import ModelCheckpoint
-
-# Constant seed for tensorflow and numpy model in order to reproduce results
-CONSTANT = 12
-
-# Dropped out 20% of neurons to prevent overfitting during each training phase
-STD_DROPOUT_LAYER = 0.25
+CONSTANT = 12 # Arbitrary constant seed such that Tensorflow and numPy can reproduce results
+STD_DROPOUT_LAYER = 0.20 # Dropped out 20% of neurons to prevent overfitting
 
 tf.random.set_seed( CONSTANT )
 np.random.seed( CONSTANT )
 
-model = Sequential()
+model = Sequential() # Important for time series data, which is sequential
 
 # Model is a Long Short-Term Memory model using the ReLU Activation model to introduce non-linearity concept to training
 model.add( LSTM ( units = 50,
                   activation = 'relu',
                   input_shape = ( training_window.shape[ 1 ], WINDOW_SIZE ) ) )
 
-model.add( Dropout ( STD_DROPOUT_LAYER ) )
-model.add( Dense( 1 ) )
-model.compile( loss = 'mean_squared_error' )
+model.add( Dropout ( STD_DROPOUT_LAYER ) ) # Dropping out 20% of neurons (random selection)
+model.add( Dense( 1 ) ) 
+model.compile( loss = 'mean_squared_error' ) # Calculating mean squared error of training and validation
 
 filepath = 'saved_models/model_epoch_{epoch:02d}.keras'
 
@@ -240,119 +224,75 @@ history = model.fit( training_window, training_target,
                      verbose = 1,
                      shuffle = False )
 
-# Recording the training losses of all epochs
-training_loss_list = history.history[ 'loss' ]
+training_loss_list = history.history[ 'loss' ] # Training MSE of model epochs
+val_loss_list = history.history[ 'val_loss' ] # Validation MSE of model epochs
 
-# Recording the validation losses of all epochs
-val_loss_list = history.history[ 'val_loss' ]
+ALPHA = 0.25 # Scoring/Accuracy formula: ALPHA * training RMSE + (1 - ALPHA) * validation RMSE
+epochs = glob.glob( PATH ) # Returns list of all model epochs generated
 
-import re
-from keras.models import load_model
-from sklearn.metrics import mean_squared_error
+training_target = scaler.inverse_transform( [ training_target ] ) # Removes scaler for plotting purposes
+val_target = scaler.inverse_transform( [ val_target ] ) # Removes scaler for plotting purposes
 
-ALPHA = 0.25
-epochs = glob.glob( PATH )
+scores = [] # Scores list meant to store statistics of each model epoch
 
-training_target = scaler.inverse_transform( [ training_target ] )
-val_target = scaler.inverse_transform( [ val_target ] )
+for i in range( len( epochs ) ):
 
-scores = []
+    training_RMSE = np.sqrt( training_loss_list[ i ] ) # Calculating RMSE of training phase
+    val_RMSE = np.sqrt( val_loss_list[ i ] ) # Calculating RMSE of validation phase
+    score = 1 / ( ALPHA * ( training_RMSE ) + ( 1 - ALPHA ) * ( val_RMSE ) ) # Scoring formula to evaluate model epoch
+    scores.append( ( str( i + 1 ), training_RMSE, val_RMSE, score ) ) # Appending statistics to score list
 
-for epoch in epochs:
-    model = load_model( epoch )
-    regex = re.search( r'model_epoch_(\d+).keras', epoch )
-    index = int( regex.group( 1 ) ) - 1
+scores_df = pd.DataFrame( scores ) # Converting to dataframe for further ease of analysis
+scores_df.columns = [ 'epoch', 'training RMSE', 'validation RMSE', 'score' ] # Changing column names
 
-    training_prediction_temp = scaler.inverse_transform( model.predict( training_window ) )
-    training_prediction_temp = np.reshape( training_prediction_temp, training_prediction_temp.shape[ 0 ] )
+print( scores_df.head() ) # Brief check
 
-    val_prediction_temp = scaler.inverse_transform( model.predict( val_window ) )
-    val_prediction_temp = np.reshape( val_prediction_temp, val_prediction_temp.shape[ 0 ] )
+optimal_index = scores_df[ 'score' ].idxmax() # Highest score means least los
+optimal_epoch_num = scores_df.iloc[ optimal_index, 0 ] # For string use, since epochs are 1-indexed
 
-    training_target_temp = np.reshape( training_target, training_prediction_temp.shape[ 0 ] )
-    val_target_temp = np.reshape( val_target, val_prediction_temp.shape[ 0 ] )
-
-    training_RMSE = np.sqrt( mean_squared_error( training_target_temp, training_prediction_temp ) )
-    val_RMSE = np.sqrt( mean_squared_error( val_target_temp, val_prediction_temp ) )
-
-    score = ALPHA * ( training_RMSE ) + ( 1 - ALPHA ) * ( val_RMSE )
-    
-    scores.append( ( str( index + 1 ), training_RMSE, val_RMSE, training_prediction_temp, val_prediction_temp, score ) )
-
-scores_df = pd.DataFrame( scores )
-scores_df.columns = [ 'epoch', 'training RMSE', 'validation RMSE', 'training prediction', 'validation prediction', 'score' ]
-
-print( scores_df )
-
-optimal_index = scores_df[ 'score' ].idxmin()
-optimal_epoch_num = scores_df.iloc[ optimal_index, 0 ]
-
-if int( optimal_epoch_num ) < 10:
+if int( optimal_epoch_num ) < 10: # Preventing index errors
     optimal_epoch_num = '0' + optimal_epoch_num
 
-optimal_model = load_model( 'saved_models/model_epoch_' + str( optimal_epoch_num ) + '.keras' )
-optimal_training_RMSE = scores_df.iloc[ optimal_index, 1 ]
-optimal_val_RMSE = scores_df.iloc[ optimal_index, 2 ]
+optimal_model = load_model( 'saved_models/model_epoch_' + str( optimal_epoch_num ) + '.keras' ) # Epoch that had the highest score
+optimal_training_RMSE = scores_df.iloc[ optimal_index, 1 ] # Training RMSE of highest score epoch
+optimal_val_RMSE = scores_df.iloc[ optimal_index, 2 ] # Validation RMSE of highest score epoch
 
-# Training phase movement of first 80% of data is predicted using optimal epoch
-# Removes the standardizing scaler of [0, 1] to return prediction to original scaling [min, max]
-training_prediction = scores_df.iloc[ optimal_index, 3 ]
-    
-# Testing phase movement of remaining 20% of data is predicted using optimal epoch
-# Removes the standardizing scale of [0, 1] to return prediction to original scaling [min, max]
-val_prediction = scores_df.iloc[ optimal_index, 4 ]
+training_prediction = scaler.inverse_transform( model.predict( training_window ) ) # training phase, forecasting 20-day window predictions
+training_prediction = np.reshape( training_prediction, training_prediction.shape[ 0 ] ) # Reshaping for concatenation with validation predictions, visualization
 
-training_target = np.reshape( training_target, ( -1, 1 ) )
-val_target = np.reshape( val_target, ( -1, 1 ) )
+val_prediction = scaler.inverse_transform( model.predict( val_window ) ) # validation phase, forecasting 20-day window predictions
+val_prediction = np.reshape( val_prediction, val_prediction.shape[ 0 ] ) # Reshaping for concatenation with training predictions, visualization
 
-# Moving into the future; no original data to compare with, attempting to predict movement 20 days in advance
-# First comparing with the last 20 days of model prediction window (final 20 days of data set comparison) to begin modeling
-last_window = prices_df[ -WINDOW_SIZE: ].reshape( ( 1, 1, WINDOW_SIZE ) )
+training_target = np.reshape( training_target, training_prediction.shape[ 0 ] ) # Reshaping for concatenation with validation target data
+val_target = np.reshape( val_target, val_prediction.shape[ 0 ] ) # Reshaping for concatenation with training validation target data
 
-# Preparing model forecast list for 20 days of prediction into the future
-forecast = []
-
-# Using final 20-day window from validation phase as a starting point for prediction
-current_window = last_window
+last_window = prices_df[ -WINDOW_SIZE: ].reshape( ( 1, 1, WINDOW_SIZE ) ) # Future predictions begin with last 20-day window of stock history data
+forecast = [] # Model epoch future forecast list of 20-day movement of price
+current_window = last_window # Using final 20-day window from validation phase as a starting point for prediction
 
 # Actual model prediction: Looking 20 days into the future and continunally predicting using a sliding window
 # There is no training/validation phase here: This is all epoch prediction
-
 for _ in range( DAYS_AHEAD ):
 
-    # Predicting next price using the current 20-day window
-    next_price = model.predict( current_window )
-
-    # Prediction price is appended to the future list for storage
-    forecast.append( next_price[ 0, 0 ] )
+    next_price = model.predict( current_window ) # Predicting next price using the current 20-day window
+    forecast.append( next_price[ 0, 0 ] ) # Prediction price is appended to the future list for storage
 
     # Current window is shifted one day in the future
     # i.e. if start index was k and end index was n, then new window is [k+1, n+1]
     current_window = np.roll( current_window, -1, axis = 2 )
-
-    # Current window now contains model predicted price for "21st" day
-    current_window[ 0, 0, -1 ] = next_price
+    current_window[ 0, 0, -1 ] = next_price # Current window now contains model predicted price for "21st" day
 
 forecast = np.array( forecast ).reshape( -1, 1 )
+forecast = scaler.inverse_transform( forecast ) # Removes scaler of [0, 1] to original interval
 
-# Resizing future list so that it removes standardizing scale of [0, 1] to original size [min, max]
-forecast = scaler.inverse_transform( forecast )
-
-# Actual prices concatenated into one list
-prices_actual = np.concatenate( ( training_target, val_target ), axis = 0 )
-
-# Model training prices concatenated into one list
-prices_model = np.concatenate( ( training_prediction, val_prediction ), axis = 0 )
-
-# Predicted prices added onto model training prices to create +20 day prediction
-forecasted_prices = np.concatenate( ( prices_model, forecast.squeeze( ) ), axis = 0 )
+prices_actual = np.concatenate( ( training_target, val_target ), axis = 0 ) # Actual prices concatenated into one list
+prices_model = np.concatenate( ( training_prediction, val_prediction ), axis = 0 ) # Model training + validation predictions concatenated into one list
+forecasted_prices = np.concatenate( ( prices_model, forecast.squeeze( ) ), axis = 0 ) # Model 20-day future prediction movements concatenated into one list
 
 STOCK_DAYS = range( len( prices_actual ) )
 FORECAST_DAYS = range( len( prices_actual ) + DAYS_AHEAD )
 EPOCHS_RANGE = range( 1, len( training_loss_list ) + 1 )
 FINAL_PRICE = str( round( forecasted_prices[ -1 ], 2 ) )
-
-import matplotlib.pyplot as plt
 
 # Preparing data visualization for performance metrics and model prediction
 fig, ( stock_plt, loss_plt ) = plt.subplots( 1, 2, figsize = ( 18, 6 ) )
